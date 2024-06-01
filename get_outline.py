194fb06092c605a6d4d265560f3d82ab3f7b72ee
takemei <ee207091@meiji.ac.jp>
2024-06-02 02:01:53 +0900

    define get_outline as func

--- get_outline.py
+++ get_outline.py
@@ -4,53 +4,58 @@ import cv2
 import numpy as np
 
 import argparse
+
+
+def get_outline(img_path):
+    # 画像を読み込む
+    img_path = args.img_path
+    img = cv2.imread(img_path)
+
+    height, width, _ = img.shape
+    print(f"shape : {height}, {width}")
+    img = img[0:int(height/2)]
+
+    '''
+    LOW_COLOR1 = np.array([32, 84, 214]) # 各最小値を指定
+    #LOW_COLOR1 = np.array([0, 0, 0]) # 各最小値を指定
+    HIGH_COLOR1 = np.array([42, 122, 250]) # 各最大値を指定
+    '''
+    LOW_COLOR1 = np.array([32, 50, 190]) # 各最小値を指定
+    HIGH_COLOR1 = np.array([42, 180, 250]) # 各最大値を指定
+
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
+    mask = cv2.inRange(hsv, LOW_COLOR1, HIGH_COLOR1) # マスクを作成
+
+
+
+    # マスク画像から輪郭を検出
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # 各輪郭を囲む長方形領域を抽出
+    extracted_regions = []
+    for contour in contours:
+        print("hey")
+        if cv2.contourArea(contour) > 100:  # ノイズを除去するために面積の閾値を設定
+            x, y, w, h = cv2.boundingRect(contour)
+            if (w*h) / (height*width) < 0.1:
+                continue
+            print((w*h) / (height*width))
+            extracted_region = img[y:y+h, x:x+w]
+            extracted_regions.append(extracted_region)
+            # 抽出した領域をデバッグ用に表示するために長方形を描画
+            # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # 抽出した領域を保存または表示
+    region = extracted_regions[0]
+    output_region_path = \
+        os.path.join("mask",os.path.basename(img_path))
+    cv2.imwrite(output_region_path, region)
+
 parser = argparse.ArgumentParser()
 parser.add_argument("img_path", help="raw_img_path is needed")
 args = parser.parse_args()
-# 画像を読み込む
-img_path = args.img_path
-img = cv2.imread(img_path)
 
-height, width, _ = img.shape
-print(f"shape : {height}, {width}")
-img = img[0:int(height/2)]
-
-'''
-LOW_COLOR1 = np.array([32, 84, 214]) # 各最小値を指定
-#LOW_COLOR1 = np.array([0, 0, 0]) # 各最小値を指定
-HIGH_COLOR1 = np.array([42, 122, 250]) # 各最大値を指定
-'''
-LOW_COLOR1 = np.array([32, 50, 190]) # 各最小値を指定
-HIGH_COLOR1 = np.array([42, 180, 250]) # 各最大値を指定
-
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
-mask = cv2.inRange(hsv, LOW_COLOR1, HIGH_COLOR1) # マスクを作成
-
-
-
-# マスク画像から輪郭を検出
-contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# 各輪郭を囲む長方形領域を抽出
-extracted_regions = []
-for contour in contours:
-    print("hey")
-    if cv2.contourArea(contour) > 100:  # ノイズを除去するために面積の閾値を設定
-        x, y, w, h = cv2.boundingRect(contour)
-        if (w*h) / (height*width) < 0.1:
-            continue
-        print((w*h) / (height*width))
-        extracted_region = img[y:y+h, x:x+w]
-        extracted_regions.append(extracted_region)
-        # 抽出した領域をデバッグ用に表示するために長方形を描画
-        # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-# 抽出した領域を保存または表示
-region = extracted_regions[0]
-output_region_path = \
-    os.path.join("mask",os.path.basename(img_path))
-cv2.imwrite(output_region_path, region)
-
+get_outline(args.img_path)
 '''
 # 結果の画像を保存
 output_image_path = 'detected_regions.jpg'
